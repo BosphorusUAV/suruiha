38d170c42eeb9cbc9c00c0c87d4561d45ec68fed
Utku <43106673+HutkuC@users.noreply.github.com>
2021-05-08 21:45:43 +0300

    Update task11.py

--- bosphorusuav_package/scripts/task11.py
+++ bosphorusuav_package/scripts/task11.py
@@ -3,14 +3,13 @@ from uav import UAV, uavs, timeHelper
 from formation import Formation
 from swarm import Swarm, swarms
 
-n = 15
+n = 5
 for uav in uavs[n:]:
     uav.land(z=0, duration=2, sleep=0)
 
-uavs = uavs[:15]
+uavs = uavs[:n]
 
-duration=30
-d = 10
+duration=5
 
 swarm0 = Swarm(uavs)
 
@@ -23,31 +22,31 @@ swarm0.changeFormation(
     uav_distance=1, 
     center=Point(None, None, 1)
 )
-swarm0.command(duration=3, inorder=0, sleep=10)
+swarm0.command(duration=3, inorder=0, sleep=5)
 
-swarm0.navigation(Point(1, 0, 0), relative=True)
-swarm0.command(duration=4, inorder=0, sleep=10)
+swarm0.navigation(Point(5, 0, 0), relative=True)
+swarm0.command(duration=4, inorder=0, sleep=5)
 
-swarm0.navigation(Point(0, 1, 0), relative=True)
-swarm0.command(duration=4, inorder=0, sleep=10)
+swarm0.navigation(Point(0, 5, 0), relative=True)
+swarm0.command(duration=4, inorder=0, sleep=5)
 
-swarm0.navigation(Point(-1, 0, 0), relative=True)
-swarm0.command(duration=4, inorder=0, sleep=10)
+swarm0.navigation(Point(-5, 0, 0), relative=True)
+swarm0.command(duration=4, inorder=0, sleep=5)
 
-swarm0.navigation(Point(0, -1, 0), relative=True)
-swarm0.command(duration=4, inorder=0, sleep=10)
+swarm0.navigation(Point(0, -5, 0), relative=True)
+swarm0.command(duration=4, inorder=0, sleep=5)
 
-swarm0.navigation(Point(1, 1, 0), relative=True)
-swarm0.command(duration=4, inorder=0, sleep=10)
+swarm0.navigation(Point(5, 5, 0), relative=True)
+swarm0.command(duration=4, inorder=0, sleep=5)
 
-swarm0.navigation(Point(-1, 1, 0), relative=True)
-swarm0.command(duration=4, inorder=0, sleep=10)
+swarm0.navigation(Point(-5, 5, 0), relative=True)
+swarm0.command(duration=4, inorder=0, sleep=5)
 
-swarm0.navigation(Point(1, -1, 0), relative=True)
-swarm0.command(duration=4, inorder=0, sleep=10)
+swarm0.navigation(Point(5, -5, 0), relative=True)
+swarm0.command(duration=4, inorder=0, sleep=5)
 
-swarm0.navigation(Point(-1, -1, 0), relative=True)
-swarm0.command(duration=4, inorder=0, sleep=10)
+swarm0.navigation(Point(-5, -5, 0), relative=True)
+swarm0.command(duration=4, inorder=0, sleep=5)
 
 for uav in uavs:
     uav.land(z=0, duration=2, sleep=0)
