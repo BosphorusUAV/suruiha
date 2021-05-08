a9760d9296ab5dd7ba64bd91c84fd3f85ace9e43
ozdentarikcan <82674996+ozdentarikcan@users.noreply.github.com>
2021-05-08 22:46:47 +0300

    Update task9.py

--- bosphorusuav_package/scripts/task9.py
+++ bosphorusuav_package/scripts/task9.py
@@ -5,29 +5,36 @@ from swarm import Swarm, swarms
 
 n = 9
 for uav in uavs[n:]:
-    uav.land(z=0, duration=2, sleep=0)
+    uav.land(z=0, duration=1, sleep=0)
 
 uavs = uavs[:9]
 
-duration=10
+duration = 5
 d = 10
-wait = 30
+wait = 3
 
 swarm0 = Swarm(uavs)
 
 timeHelper.sleep(duration)
 
-rotangles = [30 / 180, 45 / 180, -15 / 180,  90 / 180, -270 / 180]
+rotangles = [(np.pi) / 3, (np.pi) / 4, - (np.pi) / 6,  (np.pi) / 2, - 3 * (np.pi) / 2]
 
-swarm0.navigation(Point(0, 0, 1), relative=True)
-swarm0.command(duration=5, inorder=0, sleep=10)
+swarm0.changeFormation(
+    formation_type='ucgen',
+    uav_distance=1,
+    center=Point(None, None, 1)
+)
+swarm0.command(duration = 5, inorder=0, sleep = 2)
 
 for ang in rotangles :
-    x = ang / (15/180)
-    for i in range((int)x):
-        swarm0.rotation(15/180)
+    x = ang / ((np.pi) / 36)
+    for i in range(int(x)):
+        swarm0.rotation((np.pi) / 36)
         swarm0.command(duration=5, inorder=0, sleep=0.05)
     timeHelper.sleep(wait)
+
+swarm0.navigation(Point(0,0,-1) ,relative = True)
+swarm0.command(duration=d, inorder=0, sleep = 0)
 
 for uav in uavs:
     uav.land(z=0, duration=2, sleep=0.5)
