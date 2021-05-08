2e8c476d4d7fd318e787eeea8474093fd58eed36
ozdentarikcan <82674996+ozdentarikcan@users.noreply.github.com>
2021-05-08 21:05:25 +0300

    Add files via upload

---
+++ bosphorusuav_package/scripts/tasks/task9.py
@@ -0,0 +1,33 @@
+from point import Point
+from uav import UAV, uavs, timeHelper
+from formation import Formation
+from swarm import Swarm, swarms
+
+n = 9
+for uav in uavs[n:]:
+    uav.land(z=0, duration=2, sleep=0)
+
+uavs = uavs[:9]
+
+duration=30
+d = 10
+
+swarm0 = Swarm(uavs)
+
+timeHelper.sleep(duration)
+
+rotangles = [30 / 180, 45 / 180, -15 / 180,  90 / 180, -270 / 180]
+
+swarm0.navigation(Point(0, 0, 1), relative=True)
+swarm0.command(duration=5, inorder=0, sleep=10)
+
+for ang in rotangles :
+    x = ang / (15/180)
+    for i in range(x):
+        swarm0.rotation(15/180)
+        swarm0.command(duration=5, inorder=0, sleep=0.05)
+    timeHelper.sleep(wait)
+
+for uav in uavs:
+    uav.land(z=0, duration=2, sleep=0.5)
+timeHelper.sleep(5)
