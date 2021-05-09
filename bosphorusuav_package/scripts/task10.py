cf97390c3594cc621775c4b9a3c45bd9c4ecaabe
Utku <43106673+HutkuC@users.noreply.github.com>
2021-05-09 15:40:23 +0300

    Update task10.py

--- bosphorusuav_package/scripts/task10.py
+++ bosphorusuav_package/scripts/task10.py
@@ -12,18 +12,36 @@ uavs = uavs[:n]
 
 swarm0 = Swarm(uavs)
 
-timeHelper.sleep(4)
+timeHelper.sleep(10)
 
 inventory = ['ucgen', 'kare', 'besgen', 'yildiz', 'V', 'hilal']
 
-for formation_type in inventory:
-    swarm0.changeFormation( 
-        formation_type=formation_type, 
-        uav_distance=1, 
-        center=Point(None, None, 1)
-    )
-    swarm0.command(duration=3, inorder=0, sleep=5)
+for uav in uavs:
+    uav.land(z=1, duration=2.7, sleep=0)
 
+#yildiz formasyonu
+swarm0.changeFormation(
+    formation_type='yildiz', 
+    uav_distance=1, 
+    center=Point(None, None, 1)
+)
+swarm0.command(duration=2.7, inorder=0, sleep=15)
+
+#besgen formasyonu
+swarm0.changeFormation(
+    formation_type='besgen', 
+    uav_distance=1, 
+    center=Point(None, None, 1)
+)
+swarm0.command(duration=2.7, inorder=0, sleep=15)
+
+#V formasyonu
+swarm0.changeFormation(
+    formation_type='V', 
+    uav_distance=1, 
+    center=Point(None, None, 1)
+)
+swarm0.command(duration=2.7, inorder=0, sleep=15)
 
 for uav in uavs:
     uav.land(z=0, duration=2, sleep=0)
