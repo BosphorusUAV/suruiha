5250eb56fd5d78b38ec7e9fc1c9d96919e81985a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-05-09 15:38:18 +0300

    süre

--- bosphorusuav_package/scripts/task3.py
+++ bosphorusuav_package/scripts/task3.py
@@ -8,13 +8,9 @@ for uav in uavs[n:]:
     uav.land(z=0, duration=0, sleep=0)
 
 uavs = uavs[:9]
-
-duration=10
-d = 5
-
 swarm0 = Swarm(uavs)
 
-timeHelper.sleep(duration)
+timeHelper.sleep(10)
 
 inventory = ['ucgen', 'kare', 'besgen', 'yildiz', 'V', 'hilal']
 
@@ -23,25 +19,25 @@ swarm0.changeFormation(
     uav_distance=1, 
     center=Point(None, None, 1)
 )
-swarm0.command(duration=d, inorder=0, sleep=duration)
+swarm0.command(duration=2.5, inorder=0, sleep=10)
 
 for uav in uavs:
     p = uav.getPosition()
-    uav.goTo(Point(p.x, p.y, 1.5), duration=d)
+    uav.goTo(Point(p.x, p.y, 1.5), duration=2.5)
 
-timeHelper.sleep(duration)
-
-for uav in uavs:
-    p = uav.getPosition()
-    uav.goTo(Point(p.x, p.y, 1.0), duration=d)
-
-timeHelper.sleep(duration)
+timeHelper.sleep(5)
 
 for uav in uavs:
     p = uav.getPosition()
-    uav.goTo(Point(p.x, p.y, 0.1), duration=d)
+    uav.goTo(Point(p.x, p.y, 1.0), duration=2.5)
 
-timeHelper.sleep(duration)
+timeHelper.sleep(5)
+
+for uav in uavs:
+    p = uav.getPosition()
+    uav.goTo(Point(p.x, p.y, 0.1), duration=2.5)
+
+timeHelper.sleep(5)
 
 for uav in uavs:
     uav.land(z=0, duration=2, sleep=0)
